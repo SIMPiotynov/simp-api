e7fda78f6c344cb140e4dd8b7932381ee2ad0fd1
Saluc00 <lucas.usereau@ynov.com>
2023-01-12 11:29:52 +0100

    create routes of History

--- myapp/my-project/src/Controller/HistoryController.php
+++ myapp/my-project/src/Controller/HistoryController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\History;
+use App\Repository\HistoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HistoryController extends AbstractController
 {
-    #[Route('/history', name: 'app_history')]
-    public function index(): Response
+    #[Route('/history', name: 'getHistories')]
+    public function getHistories(HistoryRepository $historyRepository, SerializerInterface $serializer): Response
     {
-        return $this->render('history/index.html.twig', [
-            'controller_name' => 'HistoryController',
+        $histories = $historyRepository->findAll();
+        $histories = $serializer->serialize($histories, 'json', ['groups'=> ["history", "user"]]);
+
+        return $this->json([
+            'code' => 200,
+            'message' => json_decode($histories)
+        ]);
+    }
+
+    #[Route('/history/{id}', name: 'getHistoryById')]
+    public function getHistoryById(History $history, HistoryRepository $historyRepository, SerializerInterface $serializer): Response
+    {
+        $history = $serializer->serialize($history, 'json', ['groups'=> ["history", "user"]]);
+
+        return $this->json([
+            'code' => 200,
+            'message' => json_decode($history)
         ]);
     }
 }
