d7da8f1c30cea10c0040eb4d57f4859f5636b7e2
Saluc00 <lucas.usereau@ynov.com>
2023-01-13 14:08:46 +0100

    fix history routes

--- myapp/my-project/src/Controller/HistoryController.php
+++ myapp/my-project/src/Controller/HistoryController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use App\Entity\History;
 use App\Repository\HistoryRepository;
+use App\Service\Serializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class HistoryController extends AbstractController
 {
     #[Route('/history', name: 'getHistories')]
-    public function getHistories(HistoryRepository $historyRepository, SerializerInterface $serializer): Response
+    public function getHistories(HistoryRepository $historyRepository, Serializer $serializer): Response
     {
+        $serializer = $serializer->getSerializer();
+
         $histories = $historyRepository->findAll();
         $histories = $serializer->serialize($histories, 'json', ['groups'=> ["history", "user"]]);
 
@@ -24,8 +26,10 @@ class HistoryController extends AbstractController
     }
 
     #[Route('/history/{id}', name: 'getHistoryById')]
-    public function getHistoryById(History $history, SerializerInterface $serializer): Response
+    public function getHistoryById(History $history, Serializer $serializer): Response
     {
+        $serializer = $serializer->getSerializer();
+
         $history = $serializer->serialize($history, 'json', ['groups'=> ["history", "user"]]);
 
         return $this->json([
